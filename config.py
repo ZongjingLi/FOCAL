42322dfac69c2d09c51dee44dda4ec9bd079ae80
Melkor <115195620+ZongjingLi@users.noreply.github.com>
2023-02-20 09:31:33 +0800

    Update config.py

--- config.py
+++ config.py
@@ -5,6 +5,7 @@ device = "cuda:0" if torch.cuda.is_available() else "cpu"
 import argparse
 
 parser = argparse.ArgumentParser()
+parser.add_argument("--device",     default = device)
 parser.add_argument("--concept_dim",default = 100)
 
 config = parser.parse_args(args = [])
