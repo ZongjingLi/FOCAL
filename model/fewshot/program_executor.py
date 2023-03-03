bf678cb1e6eb25c3e89e8c8333817b8d6ada0f30
Melkor <115195620+ZongjingLi@users.noreply.github.com>
2023-03-03 10:54:47 +0800

    Update program_executor.py

--- model/fewshot/program_executor.py
+++ model/fewshot/program_executor.py
@@ -6,3 +6,24 @@ import torch.nn.functional as F
 
 from model.nn import build_entailment
 from utils import underscores, freeze
+
+
+class FewshotProgramExecutor(nn.Module):
+    NETWORK_REGISTRY = {}
+
+    def __init__(self,config):
+        super().__init__()
+        network = self.NETWORK_REGISTRY(config.name)(config)
+        entailment = build_entailment(config)
+        self.learner = PipelineLearner(network,entailment)
+
+    def forward(self,q): return q(self.learner)
+
+class MetaLearner(nn.Module):
+    pass
+
+class PipelinerLearner(nn.Module):
+    def __init__(self,network,entailment):
+        super().__init__()
+
+    
